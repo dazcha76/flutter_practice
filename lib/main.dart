a9941ade26c731d1b584f5003c4820908145fa6f
Danika <dazcha@hotmail.com>
2026-03-12 11:59:44 +0000

    created basic custom widget and displayed in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/stateless_widget.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(body: Center(child: Text('Flutter Practice'))),
+      home: Scaffold(body: Center(child: CustomTextWidget())),
     ),
   );
 }
---
+++ lib/stateless_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomTextWidget extends StatelessWidget {
+  const CustomTextWidget({super.key});
+
+  @override
+  Widget build(context) {
+    return Text('Custom text widget');
+  }
+}
