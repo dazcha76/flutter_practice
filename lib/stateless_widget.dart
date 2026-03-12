2ce3507016af45b2ae10ac6e934521e99f305f1c
Danika <dazcha@hotmail.com>
2026-03-12 12:40:26 +0000

    practiced with named parameters - have to figure out how not to display null

--- lib/stateless_widget.dart
+++ lib/stateless_widget.dart
@@ -3,8 +3,28 @@ import 'package:flutter/material.dart';
 class CustomTextWidget extends StatelessWidget {
   const CustomTextWidget({super.key});
 
+  String namedParameters({required String name, String? lastName = 'Prince'}) {
+    return '$name $lastName';
+  }
+
   @override
   Widget build(context) {
-    return Text('Custom text widget');
+    return Column(
+      children: [
+        Text('Custom text widget'),
+        Row(
+          children: [
+            Text("Both variables:"),
+            Text(namedParameters(lastName: 'Quinteros', name: 'Danika')),
+          ],
+        ),
+        Row(
+          children: [
+            Text("Only name (has default last name):"),
+            Text(namedParameters(name: 'Danika')),
+          ],
+        ),
+      ],
+    );
   }
 }
