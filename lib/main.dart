480218f014391167e627c4b9ef68e6637c94f2cf
Danika <dazcha@hotmail.com>
2026-03-12 12:19:30 +0000

    added basic stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/stateful_widget.dart';
 import 'package:flutter_practice/stateless_widget.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(body: Center(child: CustomTextWidget())),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [CustomTextWidget(), DisplayName()],
+          ),
+        ),
+      ),
     ),
   );
 }
---
+++ lib/stateful_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class DisplayName extends StatefulWidget {
+  const DisplayName({super.key});
+
+  @override
+  State<DisplayName> createState() {
+    return _DisplayNameState();
+  }
+}
+
+class _DisplayNameState extends State<DisplayName> {
+  String name = 'Danika';
+
+  void updateName() {
+    setState(() {
+      name = 'Wonder Woman';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Text(name);
+  }
+}
